601163683b371d0b0f6a3ccd8a5c76354569da77
salman_95 <salman@yarddiant.com>
2024-10-12 13:45:35 +0530

    card ui

--- src/app/ui/card/card.jsx
+++ src/app/ui/card/card.jsx
@@ -6,8 +6,8 @@ function Card({ data }) {
     <div className="row">
       {data.map((item, index) => {
         return(
-            <div className="col-md-3">
-            <div className="cs_iconbox cs_style_10 cs_radius_20 cs_white_bg text-center" key={index}>
+            <div className="col-md-3" key={index}>
+            <div className="cs_iconbox cs_style_10 cs_radius_20 cs_white_bg text-center" >
           <h2 className="cs_iconbox_title">{item.title}</h2>
           <p className="cs_iconbox_subtitle mb-0">{item.Count}</p>
         </div>
